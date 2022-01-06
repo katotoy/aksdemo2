2df3f64dcb561d308acb4ec0d6d55e0858ad7365
syril pagunsan <syril.pagunsan@godigitalcorp.com>
2022-01-06 13:55:44 +0800

    initial commt

---
+++ src/test/java/com/yahoo/cybertactics/aksdemo/AksdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yahoo.cybertactics.aksdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AksdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
