d36a47abd86e091f26ce1cefb0a1ad4f1d8bd0ab
syril pagunsan <syril.pagunsan@godigitalcorp.com>
2022-01-10 00:23:04 +0800

    test

--- src/main/java/com/yahoo/cybertactics/aksdemo/controller/HelloController.java
+++ src/main/java/com/yahoo/cybertactics/aksdemo/controller/HelloController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping
     public String hello(){
-       return "Hello World with release pipeline";
+       return "Hello World with release pipeline v1";
    }
 }
